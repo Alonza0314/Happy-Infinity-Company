ca88ef1854969b2509a802c906b60ccfbeb2a7ad
AlonzazazaProMAX <a0984078318@gmail.com>
2024-07-12 11:09:57 +0800

    update via git

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,12 @@
 function Language() {
+    var rh = document.getElementById("right-half");
+    var rhen = document.getElementById("right-half-en");
     
+    if (rh.style.display === "none") {
+        rh.style.display = "flex";
+        rhen.style.display = "none";
+    } else {
+        rh.style.display = "none";
+        rhen.style.display = "flex";
+    }
 }
