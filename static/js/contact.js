76e98f0edfe364f12ddffafcfa6cb13945e6eedd
AlonzazazaProMAX <a0984078318@gmail.com>
2024-07-14 09:15:35 +0800

    update via git

---
+++ static/js/contact.js
@@ -0,0 +1,12 @@
+function Language() {
+    var ci = document.getElementById("contact-info");
+    var cien = document.getElementById("contact-info-en");
+    
+    if (ci.style.display === "none") {
+        ci.style.display = "block";
+        cien.style.display = "none";
+    } else {
+        ci.style.display = "none";
+        cien.style.display = "block";
+    }
+}
