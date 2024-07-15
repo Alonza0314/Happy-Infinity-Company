956ab15e47b9aa075bd84a7e6c646f5c9c7df0ba
AlonzazazaProMAX <a0984078318@gmail.com>
2024-07-15 11:02:18 +0800

    update via git

--- static/js/sign.js
+++ static/js/sign.js
@@ -1,3 +1,16 @@
+function Language() {
+    var rh = document.getElementById("right-half");
+    var rhen = document.getElementById("right-half-en");
+    
+    if (rh.style.display === "none") {
+        rh.style.display = "flex";
+        rhen.style.display = "none";
+    } else {
+        rh.style.display = "none";
+        rhen.style.display = "flex";
+    }
+}
+
 let signin = document.getElementById('signin');
 let signup = document.getElementById('signup');
 let form_box = document.getElementsByClassName('form-box')[0];
